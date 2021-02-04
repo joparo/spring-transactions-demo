27310461533076a3f647202eae58cd2f8bcf2259
Jonas Paro <jonas.paro@omegapoint.se>
2021-02-04 21:32:59 +0100

    Test transactions

---
+++ src/test/java/org/joparo/spring/demo/DemoServiceTest.java
@@ -0,0 +1,46 @@
+package org.joparo.spring.demo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class DemoServiceTest {
+
+    @Autowired
+    DemoRepository repo;
+
+    @Autowired
+    DemoService service;
+
+    @BeforeEach
+    public void clear() {
+        repo.deleteAll();
+    }
+
+    @Test
+    public void testTransactionRollback() {
+        assertTrue(repo.findAll().isEmpty());
+        try {
+            service.sparaDemoTransactionalException();
+        } catch (RuntimeException e) {
+            assertTrue(repo.findAll().isEmpty());
+        }
+        assertTrue(repo.findAll().isEmpty());
+    }
+
+    @Test
+    public void testTransactionNoRollback() {
+        assertTrue(repo.findAll().isEmpty());
+        try {
+            service.sparaDemoNonTransactionalException();
+        } catch (RuntimeException e) {
+            assertFalse(repo.findAll().isEmpty());
+        }
+        assertFalse(repo.findAll().isEmpty());
+    }
+
+}
